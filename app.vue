97cf4baec86f4b70e945cdf71a83b3de6996eb82
Andrianirija Lafatra <andrianirijanyavo@gmail.com>
2025-06-01 03:31:12 +0300

    responsive

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen bg-gray-50 flex items-center justify-center p-1 md:p-4">
     <div class="w-full lg:w-1/2 bg-white rounded-lg shadow-lg md:p-6 p-1">
-      <h1 class="text-2xl font-bold mb-1 md:mb-6">Quiz en temps réel</h1>
+      <h1 class="text-2xl font-bold mb-6">Quiz en temps réel</h1>
       <QuizDisplay />
     </div>
     <Toast />
